798f2f7f5ac427c0a65aca158a3201bf5246e44a
ΛＳＬΛ Ｍ <aslampnly15@gmail.com>
2023-09-08 17:53:59 +0530

    Update downloader-git.js

--- plugins/downloader-git.js
+++ plugins/downloader-git.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch'
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 
 let handler = async (m, {conn, text, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Example user ${usedPrefix}${command} xxirfanx/zoromd`
+    if (!args[0]) throw `Example user ${usedPrefix}${command} false-eye/zoromd`
     let [usr, rep] = text.split`/`
     let url = `https://api.github.com/repos/${encodeURIComponent(usr)}/${encodeURIComponent(rep)}/zipball`
     let name = `${encodeURIComponent(rep)}.zip`
-    m.reply(`D o w n l o a d i n g. . .`)
+    m.reply(`downloading...`)
     conn.sendFile(m.chat, url, name, null, m)
 }
 
